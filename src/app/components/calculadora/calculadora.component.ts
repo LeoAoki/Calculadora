9f9d2a6abd8b33f0f3c2e0b65d128638ea23747b
Leonardo Aoki <leonardoyaoki@gmail.com>
2021-11-23 11:18:21 -0300

    Calculadora funcionando

--- src/app/components/calculadora/calculadora.component.ts
+++ src/app/components/calculadora/calculadora.component.ts
@@ -6,10 +6,63 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calculadora.component.css']
 })
 export class CalculadoraComponent implements OnInit {
+  resultadoExibir: string = ""
+  valor1: number = 0
+  valor2: number = 0
+  operacao: string = ""
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  addNumero(numero: string) {
+    this.resultadoExibir += numero;
+    if(this.valor1 == 0)
+      this.valor1 = Number(this.resultadoExibir)
+    else {
+      this.valor2 = Number(this.resultadoExibir)
+    }
+  }
+
+  addOperacao(operacao: string) {
+    this.resultadoExibir = ""
+    this.operacao = operacao
+  }
+
+  resultado() {
+    switch(this.operacao) {
+      case "+": {
+          this.resultadoExibir = String(this.valor1 + this.valor2)
+        break;
+      }
+      case "-": {
+        this.resultadoExibir = String(this.valor1 - this.valor2)
+         break;
+      }
+      case "*": {
+        this.resultadoExibir = String(this.valor1 * this.valor2)
+        break;
+      }
+      case "/": {
+        this.resultadoExibir = String(this.valor1 / this.valor2)
+        break;
+      }
+      default: {
+      break;
+      }
+    }
+    this.valor1 = Number(this.resultadoExibir)
+    this.valor2 = 0
+    this.operacao = ""
+
+  }
+
+  limpar() {
+    this.resultadoExibir = ""
+    this.valor1 = 0
+    this.valor2 = 0
+    this.operacao = ""
   }
 
 }
